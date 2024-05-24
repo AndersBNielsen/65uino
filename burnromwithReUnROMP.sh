bb9ad2450a335d6de9b3d2384011de69bcc13385
AndersBNielsen <forstuvning@gmail.com>
2024-05-24 14:12:41 +0200

    Move ROM programmer scripts to that repo.
    Scripts moved to https://github.com/AndersBNielsen/Relatively-Universal-ROM-Programmer

--- burnromwithReUnROMP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 send_command.py /dev/cu.usbserial-* 57600 03 DA 08  # 03 = erase, DA = Winbond, 08 = W27C512
-# After sending data there's a timeout before loading happens, so we need a delay
-sleep 2 
-python3 send_binary.py /dev/cu.usbserial-* 57600 build/abn6507rom.bin 128 
-sleep 2
-#Read back the ROM we just programmed to check if it's good  
-python3 read_binary.py /dev/cu.usbserial-* 57600 verify.bin 4096 128 
-diff -s build/abn6507rom.bin verify.bin
